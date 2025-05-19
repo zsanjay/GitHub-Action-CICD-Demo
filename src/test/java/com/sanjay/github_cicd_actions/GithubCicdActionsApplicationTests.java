6ca2fae8571adf583e4060ff555bebcd0fca985d
Sanjay Mehta <sanjay.mehta@hidubai.com>
2025-05-19 11:00:34 +0400

    code

---
+++ src/test/java/com/sanjay/github_cicd_actions/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.github_cicd_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
